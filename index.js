85e79b1f9331ac0cd8846e669d9a2f13ae37e837
dynamiteroll <adelprado548@gmail.com>
2023-01-12 10:57:36 -0800

    initial code for number trivia

---
+++ index.js
@@ -0,0 +1,33 @@
+const { response } = require("express");
+const express = require("express");
+
+const app = express();
+app.use(express.static('client'));
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 8000;
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
+app.post('/chat', (req, res) => {
+    console.log(req.body)
+    const message = req.body.message;
+    const number = message.match(/\d+/);
+    if (number) {
+        fetch(`http://numbersapi.com/${number}?type=trivia`).then(response => response.text()).then(data => {
+            res.json({
+                text: data
+            });
+
+        }).catch(error => {
+            res.json({
+                text: "No info about number found."
+            });
+        })
+    } else {
+        res.json({
+            text: "Please provide number."
+        });
+    }
+} )
