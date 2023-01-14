5ca5028d37f8e0a992566f248a5a95bad1779383
dynamiteroll <adelprado548@gmail.com>
2023-01-14 13:10:17 -0800

    vanilla js conversion complete, CORS test

--- client/script.js
+++ client/script.js
@@ -1,27 +1,35 @@
-$(document).ready(function() {
-	$('#form').submit(function(event) {
-		event.preventDefault();
-		const message = $('#input').val();
-        $.ajax({
-            type: 'POST',
-			url: 'https://adp4537lab2.onrender.com/chat',
-			data: {
-				message: message
-			},
-			success: function(response) { 
-                let newMessage = $('<div>', {
-                    class: 'message'
-                }).text(response.text);
-                let removeButton = $('<button>', {
-                    class: 'remove-button'
-                }).text('Remove');
-                newMessage.append(removeButton);
-                $('#history').append(newMessage);      
-			}
-        })
-	});
-});
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('form').addEventListener('submit', function (event) {
+      event.preventDefault();
+      const message = document.getElementById('input').value;
+      fetch('https://adp4537lab2.onrender.com/chat', {
+      //fetch('http://localhost:8000/chat', {
+        method: 'POST',
+        body: JSON.stringify({ message: message }),
+        headers: {
+          'Content-Type': 'application/json',
+          'Origin' : 'https://adp4537lab2.onrender.com',
+          'Access-Control-Allow-Origin' : '*'
 
-$(document).on('click', '.remove-button', function() {
-	$(this).parent().remove();
-});
+        }
+      })
+        .then(response => response.json())
+        .then(response => {
+          console.log(response);
+          const newMessage = document.createElement('div');
+          newMessage.classList.add('message');
+          newMessage.innerHTML = response.text;
+          const removeButton = document.createElement('button');
+          removeButton.classList.add('removebtn');
+          removeButton.innerHTML = 'Remove';
+          newMessage.appendChild(removeButton);
+          document.getElementById('history').appendChild(newMessage);
+        })
+        .catch(error => console.error('Error:', error));
+    });
+    document.addEventListener('click', function (event) {
+      if (event.target.classList.contains('removebtn')) {
+        event.target.parentNode.remove();
+      }
+    });
+  });
